1065053dc8601fa33b37c82567a79682b7574b5b
Michel Hathier <mhathiergmail.com>
2023-06-14 17:40:16 +0200

    Creation Products entity

---
+++ migrations/Version20230614145715.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230614145715 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, model VARCHAR(50) NOT NULL, image VARCHAR(100) NOT NULL, price DOUBLE PRECISION NOT NULL, color VARCHAR(25) NOT NULL, screen_size VARCHAR(25) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE products');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,67 @@
+# Bilmo
+
+Bilmo is a API REST designed to allow all online sales platforms to access a supplier's catalog of high-end mobile phones.
+
+Project 7 of the Openclassrooms training "PHP/Symfony Application Developper".
+
+## Tech Stack
+
+* Symfony 6.3, Api Platform 3.1
+
+## Launch
+
+* Wampserver 64bit 3.2.6
+* MySQL 5.7.36
+* github/aerial978
+
+## Set Up
+
+* Librairies Installation
+
+```bash
+    composer install
+```
+
+* Git clone the project
+
+```bash
+    https://github.com/aerial978/bilmo.git
+```
+
+* Database
+
+Update .env file your database configuration
+
+```bash
+    DATABASE_URL="mysql://db_user:db_password@127.0.0.1:3306/db_name"
+```
+
+* Create database
+
+```bash
+    php bin/console doctrine:database:create
+```
+
+* Create an entity
+
+```bash
+    php bin/console --dev symfony/maker-bundle
+    php bin/console make:entity
+```
+
+* Create database structure
+
+```bash
+    php bin/console make:migration
+```
+
+* Database up-to-date
+
+```bash
+    php bin/console doctrine:migrations:migrate
+```
+
+
+
+
+
