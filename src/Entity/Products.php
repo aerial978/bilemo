4421930bb5de32a2aa1600fde135894b075a58f7
Michel Hathier <mhathiergmail.com>
2023-06-16 17:40:32 +0200

    Creation products endpoints

--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ProductsRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource()]
 #[ORM\Entity(repositoryClass: ProductsRepository::class)]
 class Products
 {
